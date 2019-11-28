b8fea7c24e1cfcd3a9146966e1ae479bc1fcc858
Bastian <bastian.muratory@gmail.com>
2019-11-28 11:00:00 +0000

    fin première fonction

--- src/facile1.c
+++ src/facile1.c
@@ -1,16 +1,50 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-void EntrerParam(int *x , int *y , int *z , int *sigma , int *beta , int *rho){
+int lire_fin_ligne(){
+	int x=0; // le nombre de charactères
+	char c;
+	c=getchar();
+	while (c!='\n' && c != EOF){
+		if (!isspace(c)){
+			x++;
+		}
+		c=getchar();
+	}
+    return(x);
+}
+
+void lire_format(char format[], void*a){
+	int lu;
+	int nb;	
+	do{
+		lu=scanf(format,a);
+		nb=lire_fin_ligne();
+	}while((lu!=1)||(nb>0));
+}
+
+void lire_decimale(float *a){
+	int lu;
+	int nb;	
+	do{
+		lu=scanf("%f",a);
+		nb=lire_fin_ligne();
+	}while((lu!=1)||(nb>0));
+}
+
+
+void EntrerParam(float *x , float *y , float *z , float *sigma , float *beta , float *rho){
     printf("Entrez la coordonée X : \n");
-    entrerfloat(x);
+    lire_decimale(x);
     printf("Entrez la coordonée Y : \n");
-    entrerfloat(y);
+    lire_decimale(y);
     printf("Entrez la coordonée Z : \n");
-    entrerfloat(z);
+    lire_decimale(z);
     printf("Entrez le paramètre σ : \n");
-    entrerfloat(sigma);
+    lire_decimale(sigma);
     printf("Entrez le paramètre β : \n");
-    entrerfloat(beta);
+    lire_decimale(beta);
     printf("Entrez le paramètre ρ : \n");
-    entrerfloat(rho);
+    lire_decimale(rho);
 }
