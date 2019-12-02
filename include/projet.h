7b3f098cfa3657ab6d8ce2f2341a715fe9ac5868
Bastian <bastian.muratory@gmail.com>
2019-12-02 23:43:06 +0000

    début du rangement

--- include/projet.h
+++ include/projet.h
@@ -1,13 +1,44 @@
-#ifndef VARIABLE
-#define VARIABLE
-// Write your header file here.
+#ifndef __FONCTION__
+#define __FONCTION__
 
 typedef struct Point Point;
 struct Point{
 	float x;
 	float y;
 	float z;
+	float time;
 };
 
+typedef struct Vitesse Vitesse;
+struct Vitesse{
+	float x;
+	float y;
+	float z;
+};
+
+typedef struct Param Param;
+struct Param{
+	float sigma;
+	float rho;
+	float beta;
+};
+
+//fonctions pour les entrées
+int lire_fin_ligne();
+void lire_format(char format[], void*a);
+void lire_decimale(float *a);
+
+//fonctions pour le point
+Point initPoint(Point p);
+void affPoint(Point p);
+void saveP(Point p1,FILE* data);
+
+//fonction pour les parrametres et la position initiale
+Param parametrage(Param pa, float *Tmax, float *dt);
+Point pointInitial(Point p);
+
+//fonctions de calcul
+Vitesse vitesse(Vitesse v , Point p , Param pa);
+Point positionSuivante(Point p, Vitesse v, Param pa, float dt);
 
 #endif
